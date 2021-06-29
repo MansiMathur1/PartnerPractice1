ad533ef26ccf616c4b9e98677a5ae6d1bef5c621
Mansi Mathur <mansi.mathur2005@gmail.com>
2021-06-29 13:13:34 -0400

    Initial Commit

---
+++ PartnerPractice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartnerPractice1
+//
+//  Created by Mansi Mathur on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
